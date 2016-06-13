39ada8f01afb6387e04aa3232a3692ebb41fc28e
MaxPlay <maxplayyt@gmail.com>
2016-06-13 18:53:59 +0200

    Weapons + Character
    Pencilgun
    Character
    Stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+}
