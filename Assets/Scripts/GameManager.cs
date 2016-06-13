65c4335398ff960eb41a2491667f3c8a1659457a
MaxPlay <maxplayyt@gmail.com>
2016-06-14 00:50:19 +0200

    Sharpener Menu Prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,19 +14,33 @@ public class GameManager : MonoBehaviour
     private float enemyTimerValue;
     private float enemyTimer;
 
-    private float score;
+    [SerializeField]
+    private int score;
+
+    public int Score { get { return score; } }
+
+    [SerializeField]
+    private int lives;
+
+    public int Lives
+    {
+        get { return lives; }
+    }
+
 
     [SerializeField]
     private float scoreTimerValue;
     private float scoreTimer;
 
+    [SerializeField]
+    TextMesh scoreMesh;
+
     public void Awake()
     {
-
         instance = this;
         enemyTimer = enemyTimerValue;
         scoreTimer = scoreTimerValue;
-        score = 0f;
+        score = 0;
     }
 
     public void EnemyKilled()
@@ -53,6 +67,8 @@ public class GameManager : MonoBehaviour
             score += 100;
             scoreTimer = scoreTimerValue;
         }
+
+        scoreMesh.text = score.ToString("00000000000");
     }
 
     void Update()
@@ -66,7 +82,7 @@ public class GameManager : MonoBehaviour
     Vector3 GetRandomVector()
     {
         return new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5, 5f));
-        
+
     }
 
 }
