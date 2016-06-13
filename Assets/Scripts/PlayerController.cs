36f2c65c54b85a72ee6409c67713acdade2fd350
MaxPlay <maxplayyt@gmail.com>
2016-06-13 17:21:15 +0200

    PlayerUpdate 1
    Updated Playercontroller
    Added Projectile
    Setup for Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class PlayerController : MonoBehaviour {
-
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform GunOutput;
     private Rigidbody rb;
-    public float speed;
-    public GameObject obj;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private GameObject obj;
+    private bool shooting;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        GetRelativeRotationtoCamera();
         Move();
-        //Shoot();
-	}
+        Rotate();
+    }
+
+    public void Update()
+    {
+        shooting = Input.GetAxis("Fire") != 0;
+        Shoot();
+    }
+
+    private void Rotate()
+    {
+        float moveHorizontal = Input.GetAxis("HorizontalControllerRotation");
+        float moveVertical = Input.GetAxis("VerticalControllerRotation");
+
+        Vector3 rotationTarget = this.transform.position + new Vector3(moveHorizontal, 0, moveVertical);
+
+        Transform child = transform.GetChild(0);
+
+        child.LookAt(rotationTarget);
+    }
+
+    private void GetRelativeRotationtoCamera()
+    {
+
+    }
 
     void Move()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("HorizontalControllerMovement");
+        float moveVertical = Input.GetAxis("VerticalControllerMovement");
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical) * speed;
 
-        rb.velocity = movement * speed;
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
     }
 
     void Shoot()
     {
-        while(Input.GetButtonDown("Fire1"))
+        if (shooting)
         {
-
-            Instantiate(obj);
+            Instantiate(obj, GunOutput.position, transform.GetChild(0).rotation);
         }
     }
 
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float lifetime = 10;
+
+    [SerializeField]
+    private float speed = 2;
+
+    public void FixedUpdate()
+    {
+        lifetime -= Time.fixedDeltaTime;
+
+        if (lifetime <= 0)
+            Destroy(gameObject);
+
+        this.transform.position += this.transform.forward * speed;
+    }
+}
