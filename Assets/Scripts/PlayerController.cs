ceabc98d3feab469f9b8e24b10dd78e502af0e42
Sebastian Bos <info@criminux.com>
2016-06-13 15:31:53 +0200

    Added simple PlayerController Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Move();
+        //Shoot();
+	}
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+    }
+
+    void Shoot()
+    {
+        while(Input.GetButtonDown("Fire1"))
+        {
+
+            Instantiate(obj);
+        }
+    }
+
+}
